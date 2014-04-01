fe4bd2f2effb6044716e7d80609bf31abe3d1b77
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-04-01 08:06:46 -0300

    New resources

--- src/resource.js
+++ src/resource.js
@@ -1,12 +1,16 @@
 var s_HelloBG = "helloBG.png";
 var s_start_n = "start_n.png";
 var s_start_s = "start_s.png";
+var s_playBG = "PlayBG.png";
+var s_runner = "runner.png";
 
 var g_resources = [
     //image
     {src:s_HelloBG},
     {src:s_start_n},
-    {src:s_start_s}
+    {src:s_start_s},
+    {src:s_playBG},
+    {src:s_runner}
 
     //plist
 
