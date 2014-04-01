7e681a4efeb8784221177019ed0e374ef865929a
Vitor Mattos <vitor.mattos@caelum.com.br>
2014-04-01 08:42:45 -0300

    Animating hero

--- src/AnimationLayer.js
+++ src/AnimationLayer.js
@@ -1,20 +1,34 @@
 var AnimationLayer = cc.Layer.extend({
+    spriteSheet:null,
+    runningAction:null,
+    sprite:null,
     ctor:function () {
         this._super();
         this.init();
     },
+
     init:function () {
         this._super();
 
-        var winSize = cc.Director.getInstance().getWinSize();
-        var centerPos = cc.p(80, 85);
-        //cerate the hero sprite
-        var spriteRunner = cc.Sprite.create(s_runner);
-        spriteRunner.setPosition(centerPos);
+        // create sprite sheet
+        cc.SpriteFrameCache.getInstance().addSpriteFrames(s_runnerplist);
+        this.spriteSheet = cc.SpriteBatchNode.create(s_runner);
+        this.addChild(this.spriteSheet);
 
-        //create the move action
-        var actionTo = cc.MoveTo.create(2, cc.p(300, 85));
-        spriteRunner.runAction(cc.Sequence.create(actionTo));
-        this.addChild(spriteRunner);
+
+        // init runningAction
+        var animFrames = [];
+        for (var i = 0; i < 8; i++) {
+            var str = "runner" + i + ".png";
+            var frame = cc.SpriteFrameCache.getInstance().getSpriteFrame(str);
+            animFrames.push(frame);
+        }
+
+        var animation = cc.Animation.create(animFrames, 0.1);
+        this.runningAction = cc.RepeatForever.create(cc.Animate.create(animation));
+        this.sprite = cc.Sprite.createWithSpriteFrameName("runner0.png");
+        this.sprite.setPosition(cc.p(80, 85));
+        this.sprite.runAction(this.runningAction);
+        this.spriteSheet.addChild(this.sprite);
     }
 });
--- src/resource.js
+++ src/resource.js
@@ -2,7 +2,8 @@ var s_HelloBG = "helloBG.png";
 var s_start_n = "start_n.png";
 var s_start_s = "start_s.png";
 var s_PlayBG = "PlayBG.png";
-var s_runner = "runner.png";
+var s_runner = "running.png";
+var s_runnerplist = "running.plist";
 
 var g_resources = [
     //image
@@ -10,7 +11,9 @@ var g_resources = [
     {src:s_start_n},
     {src:s_start_s},
     {src:s_PlayBG},
-    {src:s_runner}
+    {src:s_runner},
+    {src:s_runner},
+    {src:s_runnerplist}
 
     //plist
 
